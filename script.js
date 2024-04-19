692d6a76dc305441fa9cbf2579db61c6bfafd25e
Gabe <115848472+g-nbo@users.noreply.github.com>
2024-04-19 17:14:36 -0400

    Part 2: Thinking Methodically

--- script.js
+++ script.js
@@ -44,6 +44,7 @@ function stringsLongerThan(arr, num) {
     return areLonger;
 }
 
+// Print every number between 1 and n without using loops
 function printBetween(n, start = 1) {
     
     if(start > n) {
@@ -60,3 +61,49 @@ console.log("Longest String: ", longestString(["word", "anotherWord", "different
 console.log("String Longer than num: ", stringsLongerThan(["word", "anotherWord", "differentWord", "thisWord"], 7));
 console.log("printBetween: ");
 printBetween(4);
+console.log("=====================================")
+
+// Part 2: Thinking Methodically
+
+let data = [{ id: "42", name: "Bruce", occupation: "Knight", age: "41" }, { id: "48", name: "Barry", occupation: "Runner", age: "25" }, { id: "57", name: "Bob", occupation: "Fry Cook", age: "19" }, { id: "63", name: "Blaine", occupation: "Quiz Master", age: "58" }, { id: "7", name: "Bilbo", occupation: "None", age: "111" }]
+
+// Sort the array by age.
+function sortData(){
+    let sortedByAge = data.sort((x, y) => x.age - y.age);
+    return sortedByAge;
+}
+
+// Filter the array to remove entries with an age greater than 50.
+function removeGreaterThan() {
+    let lessThan = data.filter((x) => x.age < 50);
+    return lessThan;
+}
+
+// Map the array to change the “occupation” key to “job” and increment every age by 1.
+function changeJobAndAge() {
+    
+    let mappedArr = data.map((x) => { 
+        //increment age by 1
+        x.age = parseInt(x.age) + 1
+        // change occupation key to a job key
+        x.job = x.occupation;
+        delete x.occupation;
+        return data;
+    })
+    return data;
+}
+
+// Use the reduce method to calculate the sum of the ages.
+function reduceAvgAge() {
+    let reducedAge = data.reduce((x, y) => x + y.age, 0);
+    // Then use the result to calculate the average age.
+    let avgAge = reducedAge / data.length;
+    return avgAge;
+}
+
+
+
+console.log(sortData());
+console.log(removeGreaterThan());
+console.log(changeJobAndAge());
+console.log(reduceAvgAge());
